2a68ffbccf8cdef43725f8300abbadc86428f590
John Doe <johndoe@example.com>
2024-02-18 03:10:07 +0500

    1

--- POS_SYSTEM.py
+++ POS_SYSTEM.py
@@ -13,7 +13,9 @@ root.minsize(500,500)
 root.maxsize(900,900)
 
 hamza = Label(text="I am a coder")
-
+hamzas = Label(text="I  a coder")
+hamza.tk_focusFollowsMouse
 hamza.pack()
+hamzas.pack()
 
 root.mainloop()
