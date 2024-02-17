f8018db3d845ab1facc6b1c15a8eb0cb366753e7
John Doe <johndoe@example.com>
2024-02-18 03:02:39 +0500

    ok

--- POS_SYSTEM.py
+++ POS_SYSTEM.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from sys import maxsize
 from tkinter import *
 
@@ -11,5 +12,8 @@ root.geometry("480x480")
 root.minsize(500,500)
 root.maxsize(900,900)
 
+hamza = Label(text="I am a coder")
+
+hamza.pack()
 
 root.mainloop()
