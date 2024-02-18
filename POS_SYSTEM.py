df2aacbac9fa21351a497d506b879310efec1392
John Doe <johndoe@example.com>
2024-02-18 12:25:21 +0500

    yes

--- POS_SYSTEM.py
+++ POS_SYSTEM.py
@@ -1,21 +1,2 @@
-from cProfile import label
-from sys import maxsize
-from tkinter import *
-
-
-
-root = Tk()
-
-
-
-root.geometry("480x480")
-root.minsize(500,500)
-root.maxsize(900,900)
-
-hamza = Label(text="I am a coder")
-hamzas = Label(text="I  a coder")
-hamza.tk_focusFollowsMouse
-hamza.pack()
-hamzas.pack()
-
-root.mainloop()
+  fcg bhbjn
+   global
