8bbf6bc16409bdaa081fb43dee3e148b66db23dc
HamzaIshaq <156201157+Hamzaisadev@users.noreply.github.com>
2024-02-19 22:51:49 +0500

    Update import customtkinter as ctk.py

--- import customtkinter as ctk.py
+++ import customtkinter as ctk.py
@@ -1,13 +1,19 @@
-#first i will add required module tkinter AND CUSTOM TKINTER
-# First, import the required modules
 from tkinter import *
-from turtle import width
 import customtkinter
 from customtkinter import *
+from CTkMessagebox import CTkMessagebox
+
+#first i will add required module tkinter AND CUSTOM TKINTER
+# First, import the custom tkinter module am
+
+
+
+
 
 # Set the appearance of the custom Tkinter to dark
 customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("dark-blue")  
+# Themes: "blue" (standard), "green", "dark-blue"
 
 # Create the main window (root)
 root = customtkinter.CTk()
@@ -22,15 +28,69 @@ root.title('POS System')
 root.geometry("1500x750")
 root.minsize(1400, 800)
 
-#lets 
-    
+
 def get_product():
-    # i am using .configure the property of CTk to configure ofcourse
-    product_label.configure(text=f'product {enter_product.get()} added successfully')
+    return
+
+#function to exit program connected to exit button
+
+def exit_program():
+    # Show some retry/cancel warnings
+    msg = CTkMessagebox(master=root,
+      title="Are You Sure!",
+    message="Do you want exit?",
+    icon="warning",
+    option_1="Yes",
+    option_2= "No")
+    yes_or_no = msg.get()
+    if yes_or_no == "Yes":
+        root.destroy()
+    else:
+        print("press yes to exit")
+
+
+def calculate_total():
+  print()
+  
+    # Add your logic here for calculating total
+
+
+def login():
+  username = enter_username.get()
+  password = enter_password.get()
+  if username == "admin" and password == "admin":
+    admin = customtkinter.CTkToplevel()
+    admin.geometry("1200x550")
+    admin.title("Admin Page")
     
+  elif username == "" and password == "":
+    CTkMessagebox(title="Error", message="Please enter username and password", icon="error")
+
+
+  elif username == "":
+    CTkMessagebox(title="Error", message="Please enter username", icon="error")
+
+  elif password == "":
+    CTkMessagebox(title="Error", message="Please enter password", icon="error")
+
+  else:
+    CTkMessagebox(title="Error", message="Invalid username or password", icon="error")
+  
     
+
+
+def print_bill():
+  print()
+
+
+
+
+
+def show_history():
+  return
+
+
 # lets make buttons
-#editing buttons aswell
 
 #add to cart button
 add_to_cart = customtkinter.CTkButton(root,
@@ -52,7 +112,8 @@ exit_button = customtkinter.CTkButton(root, text ="Exit",
     text_color="black",
     fg_color="yellow",
     hover_color="#c2b84e",
-    corner_radius=200)
+    corner_radius=200,
+    command=exit_program)
 
 
 #total button
@@ -74,7 +135,8 @@ login_button = customtkinter.CTkButton(root, text ="Login",
     text_color="black",
     fg_color="yellow",
     hover_color="#c2b84e",
-    corner_radius=200)
+    corner_radius=200,
+    command=login)
 
 
 #print button
@@ -96,16 +158,6 @@ history_button = customtkinter.CTkButton(root, text ="History",
     fg_color="yellow",
     hover_color="#c2b84e",
     corner_radius=200)
-
-    # now i will pack my button and give them padding
-add_to_cart.pack(pady=20)
-exit_button.pack(pady=20)
-total_button.pack(pady=20)
-login_button.pack(pady=20)
-print_bill_button.pack(pady=20)
-history_button.pack(pady=20)
-
-
 
 
 
@@ -154,26 +206,77 @@ enter_username= customtkinter.CTkEntry(root,
 
 
 
-#packing entry boxes
-enter_product.pack(pady=10)
-enter_quantity.pack(pady=10)
-enter_username.pack(pady=10)
-enter_password.pack(pady=10)
-
-
-
 #using.CTklabels function
 #my input from entry box will fo here
 product_label = customtkinter.CTkLabel(root,
     text ="",
     font=("helvetica",19))
 
+price_label = customtkinter.CTkLabel(root,
+    text ="",
+    font=("helvetica",19))
+
+login_label = customtkinter.CTkLabel(root,
+    text ="",
+    font=("helvetica",19))
+
+password_label = customtkinter.CTkLabel(root,
+    text ="",
+    font=("helvetica",19))
+
+
+
+
+headingLabel= customtkinter.CTkLabel(root ,text='KAMAL PAINT PORTAL',
+  font=('times new roman',30,'bold'),
+  text_color='yellow',
+  fg_color='#F57D1F',
+  justify='right')
+headingLabel.pack()
+
+
+
+#packing And placing Everything
+
+#entry boxes :
+
+enter_product.pack(pady=10)
+enter_product.place(x=200,y=100)
+
+enter_quantity.pack(pady=10)
+enter_quantity.place(x=200,y=200)
+
+enter_username.pack(pady=10)
+enter_username.place(x=200,y=300)
+
+enter_password.pack(pady=10)
+enter_password.place(x=200,y=400)
+
+#buttons :
+
+add_to_cart.pack(pady=20)
+add_to_cart.place(x=200,y=500)
+
+exit_button.pack(pady=20)
+exit_button.place(x=200,y=600)
+
+total_button.pack(pady=20)
+total_button.place(x=200,y=700)
+
+login_button.pack(pady=20)
+login_button.place(x=200,y=800)
+
+print_bill_button.pack(pady=20)
+print_bill_button.place(x=200,y=900)
+
+history_button.pack(pady=20)
+history_button.place(x=200,y=1000)
+
 product_label.pack(pady=40)
-    
-    
-    
+price_label.pack(pady=40)
+login_label.pack(pady=40)
+password_label.pack(pady=40)
 
 # Start the main loop
 root.mainloop()
 #lets make a function that will show output from entry box when i click button
-    
